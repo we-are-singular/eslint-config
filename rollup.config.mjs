95f778df9a28b0d6741c52e774e07662db393cb6
Jonathan Tavares <jonathan@wearesingular.com>
2024-03-09 18:49:19 +0000

    feature: improve the build command

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,6 @@
+import virtual from "@rollup/plugin-virtual"
+import prettier from "rollup-plugin-prettier"
+
 export default [
   {
     input: "main.mjs",
@@ -5,6 +8,31 @@ export default [
     output: [
       { file: "./dist/main.cjs", format: "cjs" },
       { file: "./dist/main.mjs", format: "es" },
+    ],
+  },
+  {
+    input: "all",
+    output: {
+      file: "./all.js",
+      format: "cjs",
+    },
+    plugins: [
+      virtual({
+        all:
+          "module.exports = " +
+          JSON.stringify((await import("./dist/main.mjs")).default.presets.all, null, 2),
+      }),
+      prettier({
+        parser: "babel",
+        filepath: "all.js",
+        arrowParens: "always",
+        useTabs: false,
+        printWidth: 100,
+        tabWidth: 2,
+        singleQuote: false,
+        trailingComma: "es5",
+        semi: false,
+      }),
     ],
   },
 ]
