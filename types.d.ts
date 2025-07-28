51515af576e266ec199afa5678c908205053f5c6
Jonathan Tavares <jonathan@wearesingular.com>
2025-07-28 03:19:58 +0100

    fix: adjust types for eslint9

--- types.d.ts
+++ types.d.ts
@@ -1,8 +1,5 @@
 import type { Linter } from "eslint"
 
-export function config(...args: Array<Partial<Linter.Config>>): Linter.Config[]
-export function extend(...args: Array<Partial<Linter.Config>>): Linter.Config[]
-
 export const presets: {
   markdown: Linter.Config[]
   monorepo: Linter.Config[]
@@ -12,33 +9,26 @@ export const presets: {
   typescript: Linter.Config[]
   astro: Linter.Config[]
   yaml: Linter.Config[]
-  all: Linter.Config[]
 }
 
 export const overrides: {
-  vitestNode: (paths: string[]) => Linter.ConfigOverride
-  vitestReact: (paths: string[]) => Linter.ConfigOverride
+  vitestNode: (paths: string[]) => Linter.Config
+  vitestReact: (paths: string[]) => Linter.Config
   namingConvention: (
     paths: string[],
     names?: string[],
     overrides?: Record<string, string[]>
-  ) => Linter.ConfigOverride
+  ) => Linter.Config
   fileNamingConvention: (
     paths: string[],
     rules?: Linter.RulesRecord
-  ) => Linter.ConfigOverride
-  typescript: (
-    paths: string[],
-    config: Omit<Linter.ConfigOverride, "files">
-  ) => Linter.ConfigOverride
+  ) => Linter.Config
 }
 
 // Default export (for CommonJS compatibility and default imports)
 declare const _default: {
   presets: typeof presets
   overrides: typeof overrides
-  config: typeof config
-  extend: typeof extend
 }
 
 export default _default
